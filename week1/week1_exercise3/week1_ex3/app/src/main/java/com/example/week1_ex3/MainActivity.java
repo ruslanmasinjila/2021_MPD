f8aff23200d6fa2c34b65ec78404f239d355a5fd
ruslanmasinjila <ruslanmasinjila@gmail.com>
2021-01-18 02:32:32 +0400

    week1 exercise 3

---
+++ week1/week1_exercise3/week1_ex3/app/src/main/java/com/example/week1_ex3/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.week1_ex3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button exitButton;
+    private EditText nameEntry;
+    private TextView toChange;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        exitButton = findViewById(R.id.exitButton);
+        nameEntry =  findViewById(R.id.nameEntry);
+        toChange =  findViewById(R.id.toChange);
+
+    }
+
+
+    public void changeColor(View exitButton)
+    {
+        String name = nameEntry.getText().toString();
+        toChange.setText(name);
+        toChange.setTextColor(Color.RED);
+
+
+    }
+}
