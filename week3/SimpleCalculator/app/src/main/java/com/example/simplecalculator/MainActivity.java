05db24f892287f18b0977ff1964e36019f5a4117
ruslanmasinjila <ruslanmasinjila@gmail.com>
2021-02-07 21:09:29 +0400

    added border to textView in week3

--- week3/SimpleCalculator/app/src/main/java/com/example/simplecalculator/MainActivity.java
+++ week3/SimpleCalculator/app/src/main/java/com/example/simplecalculator/MainActivity.java
@@ -47,15 +47,10 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    public void clearResults(View v)
-    {
+    public void clearResults(View v) {
         editTextLENGTH.setText("");
         editTextHEIGHT.setText("");
         textViewRESULT.setText("");
-
-
-
-
     }
 
 
