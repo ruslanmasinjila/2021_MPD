6163d8351d910dbe31d1ca8e448bf459234960bf
ruslanmasinjila <ruslanmasinjila@gmail.com>
2021-02-11 19:38:50 +0400

    implemented Runnable and Handlers

--- week5/SimpleCalculator/app/src/main/java/com/example/simplecalculator/MainActivity.java
+++ week5/SimpleCalculator/app/src/main/java/com/example/simplecalculator/MainActivity.java
@@ -2,13 +2,19 @@ package com.example.simplecalculator;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.content.Context;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -48,13 +54,8 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    public void clearResults(View view)
-    {
-        editTextLENGTH.setText("");
-        editTextHEIGHT.setText("");
-        textViewRESULT.setText("");
-
-
+    public void clearResults(View view) {
+        asyncClearResults(view);
     }
 
     void closeKeyBoard() {
@@ -63,6 +64,66 @@ public class MainActivity extends AppCompatActivity {
             InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
             imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
         }
+    }
+
+    public void asyncClearResults(View view) {
+        ExecutorService executor = Executors.newSingleThreadExecutor();
+        Handler handler = new Handler(Looper.getMainLooper());
+
+        // Create new ProgressBar
+        ProgressDialog progressBar;
+        progressBar = new ProgressDialog(view.getContext());
+        progressBar.setCancelable(true);
+        progressBar.setMessage("Clearing Results...");
+        progressBar.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
+        progressBar.setProgress(0);
+        progressBar.setMax(5);
+        progressBar.show();
+
+        executor.execute(new Runnable() {
+
+            @Override
+            public void run() {
+
+                ///////////////////////////////////////////////////////////////////////////////////
+                //Simulate Background task
+                ///////////////////////////////////////////////////////////////////////////////////
+                for (int i = 0; i < 5; i++) {
+                    // Simulate delay
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+
+                    // Update Progress Bar
+                    progressBar.setProgress(i);
+                }
+                // Dismiss the ProgressBar
+                progressBar.dismiss();
+
+
+                ///////////////////////////////////////////////////////////////////////////////////
+                // Clear the TextView and EditText fields
+                handler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        editTextLENGTH.setText("");
+                        editTextHEIGHT.setText("");
+                        textViewRESULT.setText("");
+                    }
+                });
+                ///////////////////////////////////////////////////////////////////////////////////
+                handler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        //UI Thread work here
+                        System.out.println("UI method");
+                    }
+                });
+                ///////////////////////////////////////////////////////////////////////////////////
+            }
+        });
     }
 
 
